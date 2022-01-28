58c56a0ed2e0d476be440567ad836a173a5f04ad
nadduli <naddulidaniel94@gmail.com>
2022-01-29 02:23:23 +0300

    modified file storage

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -86,4 +86,8 @@ class DBStorage:
         Base.metadata.create_all(self.__engine)
         session_factory = sessionmaker(bind=self.__engine,
                                        expire_on_commit=False)
-        self.__session = scoped_session(session_factory)()
+        self.__session = scoped_session(session_factory)
+
+    def close(self):
+        '''closing current session'''
+        self.__session.remove()
